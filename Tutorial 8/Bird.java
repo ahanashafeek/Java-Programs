a3f3437c86951f6b4ad140e32b67d02e935c1de2
AHANA SHAFEEK <ahanashafeek21@gmail.com>
2026-09-07 22:15:22 +0530

    Create Bird.java

---
+++ Tutorial 8/Bird.java
@@ -0,0 +1,37 @@
+class Bird {
+
+    void fly() {
+        System.out.println("Bird can fly.");
+    }
+}
+
+class Sparrow extends Bird {
+
+    @Override
+    void fly() {
+        System.out.println("Sparrow flies short distances.");
+    }
+}
+
+class Eagle extends Bird {
+
+    @Override
+    void fly() {
+        System.out.println("Eagle soars high in the sky.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Bird[] birds = {
+            new Bird(),
+            new Sparrow(),
+            new Eagle()
+        };
+
+        for (Bird b : birds) {
+            b.fly();
+        }
+    }
+}
